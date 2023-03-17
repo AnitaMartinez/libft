33566dfb249e58e464d9667a221cb2cb03b25f59
AnitaMartinez <amartinezaguilar6@gmail.com>
2023-03-17 14:51:04 +0100

    wip

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 
 int	main(void)
 {
-	// printf("ft_isalpha: %d:\n", ft_isalpha('c'));
-	// printf("ft_isdigit: %d:\n", ft_isdigit('1'));
-	// printf("ft_isalnum: %d:\n", ft_isalnum('1'));
-	// printf("ft_isascii: %d", ft_isascii('2'));
-	// printf("ft_strlen: %d", ft_strlen("hola"));
-	// printf("ft_isprint: %d", ft_isprint('c'));
+	printf("ft_isalpha: %d\n", ft_isalpha('c'));
+	printf("ft_isdigit: %d\n", ft_isdigit('1'));
+	printf("ft_isalnum: %d\n", ft_isalnum('1'));
+	printf("ft_isascii: %d\n", ft_isascii('2'));
+	printf("ft_strlen: %d\n", ft_strlen("hola"));
+	printf("ft_isprint: %d\n", ft_isprint('c'));
 	return (0);
 }
