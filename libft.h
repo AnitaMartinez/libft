773bdf72eb53fb3e2c06d493fd58d16468273405
AnitaMartinez <amartinezaguilar6@gmail.com>
2023-03-21 17:55:43 +0100

    wip

--- libft.h
+++ libft.h
@@ -23,5 +23,7 @@ int	ft_isascii(int c);
 int	ft_strlen(const char *s);
 int	ft_isprint(int c);
 int	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 
 #endif
