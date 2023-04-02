13d6e746cce78ee739c03545b4699e213ecd29c9
Ana Martinez <anamart3@c1r4s6.42madrid.com>
2023-04-02 13:41:34 +0200

    wip

---
+++ ft_atoi.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamart3 <anamart3@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/02 12:03:04 by anamart3          #+#    #+#             */
+/*   Updated: 2023/04/02 13:08:16 by anamart3         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	get_sign(const char *str, int *counter)
+{
+	int	sign;
+	int	i;
+
+	i = *counter;
+	sign = 1;
+	while ((str[i] == 45 || str[i] == 43)
+		&& (str[i + 1] >= 48 && str[i + 1] <= 57))
+	{
+		if (str[i] == 45)
+			sign *= -1;
+		i++;
+	}
+	*counter = i;
+	return (sign);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	sign;
+	int	i;
+
+	result = 0;
+	i = 0;
+	sign = 1;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	sign = get_sign(str, &i);
+	while (str[i])
+	{
+		if (str[i] >= 48 && str[i] <= 57)
+		{
+			result = result * 10 + str[i] - 48;
+		}
+		else
+			break ;
+		i++;
+	}
+	return (result * sign);
+}
+
+// #include <stdlib.h>
+// #include <stdio.h>
+// int	main(void){
+// 	char str[] = "   -15?34534";
+// 	printf("Official function: %i\n", atoi(str));
+// 	printf("My function: %i\n", ft_atoi(str));
+// 	return (0);
+// }
+
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,12 @@
 /*   By: anamart3 <anamart3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 19:09:06 by anamart3          #+#    #+#             */
-/*   Updated: 2023/03/29 19:23:00 by anamart3         ###   ########.fr       */
+/*   Updated: 2023/04/02 11:45:09 by anamart3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include <stdio.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
@@ -20,10 +21,13 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 	cdst = (unsigned char *) dst;
 	csrc = (const unsigned char *) src;
+	printf("Dst %p\n", dst);
+	printf("Src %p\n", src);
 	if (!dst && !src)
 		return (0);
 	if (dst <= src)
 	{
+		printf("%s\n", "dst <= src");
 		i = 0;
 		while (i < len)
 		{
@@ -33,6 +37,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	}
 	else if (dst > src)
 	{
+		printf("%s\n", "dst > src");
 		i = len - 1;
 		while (i > 0)
 		{	
@@ -43,21 +48,21 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	return (dst);
 }
 
-// #include <stdio.h>
-// #include <string.h>
-// int main(void)
-// {
-// 	char dst[50] = "Hello";
-// 	char src[] = "world!";
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	char dst[50] = "Hello";
+	char src[] = "world!";
 
-// 	char dst1[] = "Hello";
+	char dst1[] = "Hello";
 
 
-// 	printf("Oficcial function return: %s\n", memmove(dst + 5, src, 6));
-// 	printf("Official function dst: %s\n", dst);
+	printf("Oficcial function return: %s\n", memmove(dst + 5, src, 6));
+	printf("Official function dst: %s\n", dst);
 
-// 	printf("My function return: %s\n", ft_memmove(dst1 + 5, src, 6));
-// 	printf("My function dst: %s\n", dst1);
+	printf("My function return: %s\n", ft_memmove(dst1 + 5, src, 6));
+	printf("My function dst: %s\n", dst1);
 
-// 	return (0);
-// }
+	return (0);
+}
