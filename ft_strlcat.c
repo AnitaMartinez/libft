3fced9e93428bd221f9f4bd386ebc286b67cf509
Ana Martinez <anamart3@c3r13s6.42madrid.com>
2023-04-08 15:00:44 +0200

    Fix strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,52 +6,52 @@
 /*   By: anamart3 <anamart3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 19:09:49 by anamart3          #+#    #+#             */
-/*   Updated: 2023/04/04 19:46:34 by anamart3         ###   ########.fr       */
+/*   Updated: 2023/04/08 15:00:06 by anamart3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <stddef.h>
-
-// size_t	ft_strlen(const char *s);
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	dst_length;
+	size_t	initial_dst_length;
 	size_t	src_length;
 	size_t	i_dst;
 	size_t	i_src;
 
-	dst_length = ft_strlen(dst);
+	initial_dst_length = ft_strlen(dst);
 	src_length = ft_strlen(src);
-	i_dst = dst_length;
+
+	i_dst = initial_dst_length;
 	i_src = 0;
-	while (src[i_src] && i_dst < dstsize - 1)
+	
+	if (dstsize != 0)
 	{
-		dst[i_dst] = src[i_src];
-		i_dst++;
-		i_src++;
+		while (src[i_src] && i_dst < dstsize - 1)
+		{
+			dst[i_dst] = src[i_src];
+			i_dst++;
+			i_src++;
+		}
+		dst[i_dst] = '\0';
 	}
-	dst[i_dst] = '\0';
-	if (dst_length > dstsize)
-		return (dst_length + dstsize + 1);
-	return (dst_length + src_length);
+
+	if (initial_dst_length > dstsize)
+		return (src_length + dstsize);
+	return (initial_dst_length + src_length);
 }
 
-/* #include <stdio.h>
-#include <string.h>
-int	main(void)
-{
-	char dst[50] = "hola";
-	char src[] = "mundo";
-	char dst1[50] = "hola";
-	size_t total_length = 11;
-
-	printf("Official function return: %lu\n", strlcat(dst, src, total_length)); // 6
-	printf("Official function dst: %s\n", dst); // hola
-
-	printf("My function return: %lu\n", ft_strlcat(dst1, src, total_length)); // 9
-	printf("My function dst: %s\n", dst1); // hola
-
-	return (0);
-} */
+// #include <stdio.h>
+// #include <string.h>
+// int	main(void)
+// {
+// 	char dst[] = "123456789";
+// 	char dst1[] = "123456789";
+// 	char src[] = "ana";
+// 	size_t total_length = 0;
+// 	printf("Official function return: %lu\n", strlcat(dst, src, total_length));
+// 	printf("Official function dst: %s\n", dst);
+// 	printf("My function return: %lu\n", ft_strlcat(dst1, src, total_length));
+// 	printf("My function dst: %s\n", dst1);
+// 	return (0);
+// }
