5c1b972f098a2cdede03de0b449a4cbe30d64eed
Ana Martinez <anamart3@c3r13s6.42madrid.com>
2023-04-08 17:20:20 +0200

    Fix strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,43 +6,32 @@
 /*   By: anamart3 <anamart3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 18:59:56 by anamart3          #+#    #+#             */
-/*   Updated: 2023/04/04 19:46:53 by anamart3         ###   ########.fr       */
+/*   Updated: 2023/04/08 17:17:31 by anamart3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <stddef.h>
-
-// size_t	ft_strlen(const char *s);
 #include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
-	char	tofind;
-	char	*str;
-	size_t	str_length;
-	int		i;
+	int	i;
 
-	tofind = (char)c;
-	str = (char *)s;
-	str_length = ft_strlen(s);
-	i = str_length;
-	if (c == '\0')
-		return (str + str_length);
+	i = ft_strlen(s);
 	while (i >= 0)
 	{
-		if (str[i] == c)
-			return (str + i);
+		if ((unsigned char)s[i] == (unsigned char)c)
+			return ((char *)&s[i]);
 		i--;
 	}
-	return (0);
+	return (NULL);
 }
 
 // #include <stdio.h>
 // #include <string.h>
 // int	main(void)
 // {
-//     char str[] = "";
-//     char tofynd = 'p';
+//     char str[] = "hola como estas como";
+//     char tofynd = '\0';
 //     printf("My function: %s\n", ft_strrchr(str, tofynd));
 //     printf("Official function: %s\n", strrchr(str, tofynd));
 //     return (0);
