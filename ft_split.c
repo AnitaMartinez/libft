d7fdbec6089d58482c269a8e7d3c82826d535c11
Ana Martinez <anamart3@c1r8s5.42madrid.com>
2023-04-12 19:16:29 +0200

    wip

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: anamart3 <anamart3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 19:48:54 by anamart3          #+#    #+#             */
-/*   Updated: 2023/04/12 18:13:56 by anamart3         ###   ########.fr       */
+/*   Updated: 2023/04/12 19:09:40 by anamart3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	number_of_words(char const *s, char c)
+static int	number_of_words(char const *s, char c)
 {
 	int	i;
 	int n;
@@ -32,14 +32,17 @@ char	**ft_split(char const *s, char c)
 {
 	int words_number;
 	char **strings;
-	char *string;
 	int	i;
 	int	start;
 	int	len;
 	int	index_final_array;
+	char *ptrnull;
+
+	if (!s)
+		return (NULL);
 
 	words_number = number_of_words(s, c);
-	strings = ft_calloc((size_t)words_number + 1, sizeof(char *));
+	strings = malloc((words_number + 1) * sizeof(char *));
 	if (strings == NULL)
 		return (NULL);
 
@@ -52,12 +55,13 @@ char	**ft_split(char const *s, char c)
 		if ((s[i] != c) && (s[i + 1] == c || s[i + 1] == '\0'))
 		{
 			len = (i - start) + 1;
-			string = ft_substr(s, start, len);
-			strings[index_final_array] = string;
+			strings[index_final_array] = ft_substr(s, start, len);
 			index_final_array++;
 		}
 		i++;
 	}
+	ptrnull = NULL;
+	strings[index_final_array] = ptrnull;
 	return (strings);
 }
 
