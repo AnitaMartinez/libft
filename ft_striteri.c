5a608a258bed195ef9cf0e4a15b8a7061f59e1a1
Ana Martinez <anamart3@c1r16s1.42madrid.com>
2023-04-18 19:03:49 +0200

    Fix striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -3,34 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anamartinez <anamartinez@student.42.fr>    +#+  +:+       +#+        */
+/*   By: anamart3 <anamart3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 19:29:14 by anamartinez       #+#    #+#             */
-/*   Updated: 2023/04/13 19:43:49 by anamartinez      ###   ########.fr       */
+/*   Updated: 2023/04/18 19:03:27 by anamart3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
 
 	if (!s || !f)
 		return ;
 	i = 0;
-	while (*s)
+	while (s[i])
 	{
-		f(i, s);
-		s++;
+		f(i, s + i);
+		i++;
 	}
 }
 
-// void aux_function(unsigned int i, char* str)
+// void aux_function(unsigned int i, char* c)
 // {
 // 	(void)i;
-// 	str[0] = ft_toupper(str[0]);
+// 	*c = ft_toupper(*c);
 // }
+
 // int	main(void)
 // {
 // 	char str[] = "hola";
