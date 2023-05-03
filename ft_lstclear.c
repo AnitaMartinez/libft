14ebeb43720ca13f4f7316a7e7e745dc9e6f06e7
AnitaMartinez <amartinezaguilar6@gmail.com>
2023-05-03 18:29:35 +0200

    lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -3,27 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anamart3 <anamart3@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anamartinez <anamartinez@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 20:08:55 by anamart3          #+#    #+#             */
-/*   Updated: 2023/04/26 20:15:50 by anamart3         ###   ########.fr       */
+/*   Updated: 2023/05/03 18:29:02 by anamartinez      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// typedef struct s_list
-// {
-// 	void			*content;
-// 	struct s_list	*next;
-// }	t_list;
-
-	
-void ft_lstclear(t_list **lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	if (!lst || !del)
-		return (NULL);
-	
+	t_list	*buffer;
+
+	if (!lst || !*lst || !del)
+		return ;
+	while (*lst)
+	{
+		buffer = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = buffer;
+	}
 }
 
 void free_memory_content(void *content)
@@ -31,6 +31,7 @@ void free_memory_content(void *content)
 	free(content);
 }
 
+// TODO: probarla
 int	main(void)
 {
 	t_list *my_list = NULL;
@@ -40,14 +41,12 @@ int	main(void)
 	my_list->next = node2;
 	t_list *node3 = ft_lstnew("node 3");
 	node2->next = node3;
-	
 	while (my_list != NULL)
 	{
 		printf("Before: %s\n", my_list->content);
 		my_list = my_list->next;
 	}
 
-	ft_lstclear(my_list, free_memory_content);
-	
+	ft_lstclear(&my_list, free_memory_content);
 	return (0);
 }
